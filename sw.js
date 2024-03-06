7c34dad25d7c35530f0f48ea3ee23633a2776f73
Negash <negash@cca.edu>
2024-03-06 15:00:53 -0800

    PWA TIME

---
+++ sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('pooptracker').then((cache) => cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        // Add other files you want to cache
+      ])),
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('pooptracker').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+      '/styles.css',
+      '/script.js',
+      // Add other files you want to cache
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
+  
